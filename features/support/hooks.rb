8cef8aadb108d93a1d639c3d07c9c665bd330b92
EugeniyPetrik <eugeniy.petrik@gmail.com>
2018-03-26 19:50:38 +0300

    Add screenshots

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,6 +2,23 @@ Before do
   $driver.start_driver
 end
 
-After do
+After do |scenario|
+  if scenario.failed?
+
+    FileUtils.mkdir_p("screenshot") unless File.directory?("screenshot")
+
+    screenshot_name = Time.now.strftime("%Y-%m-$d_%H.%M.%S") + ".png"
+
+    screenshot_file = File.join("screenshot", screenshot_name)
+
+    $driver.screenshot(screenshot_file)
+
+    embed("#{screenshot_file}", "image/png")
+  end
+
   $driver.driver_quit
+end
+
+AfterConfiguration do
+  FileUtils.rm_r("screenshot") if File.directory?("screenshot")
 end
